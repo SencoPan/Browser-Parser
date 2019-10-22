674baa468bc5f6f6d143edbc71f82e1407683217
Senco <sarprolts@gmail.com>
2019-10-22 22:36:37 +0300

    MultiThread
    01

---
+++ SumkinHomework/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using OpenQA.Selenium;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace SumkinHomework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public static IWebDriver Browser;
+        public static IWebDriver Browser1;
+        String pathToJSON = @"C:\Users\Sepon\source\repos\SumkinHomework\SumkinHomework\bin\Debug/post.json";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void test() 
+        {
+            Browser1 = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser1.Manage().Window.Maximize();
+
+            Browser1.Navigate().GoToUrl("https://habr.com/en");
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread th = new Thread(test);
+            th.Start();
+
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Manage().Window.Maximize();
+
+            Browser.Navigate().GoToUrl("https://habr.com/en/top/weekly/");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        { 
+            List<IWebElement> elements = Browser1.FindElements(By.CssSelector(".content-list__item_post article h2 a")).ToList();
+            elements[1].Click();
+
+            System.Threading.Thread.Sleep(1000);
+
+            News currentPost = new News(Browser1);
+
+            textBox1.AppendText(currentPost.Article);
+            textBox2.AppendText(currentPost.Content);
+
+            currentPost.Links.ForEach(x => textBox3.AppendText("\n Cсылка:" + x + "\n"));
+            currentPost.Source.ForEach(x => textBox3.AppendText("\n Путь к картинке:" + x  + "\n"));
+
+
+            JsonSerializer serializer = new JsonSerializer();
+            string output = JsonConvert.SerializeObject(currentPost);
+
+            using (StreamWriter sw = new StreamWriter(pathToJSON))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, output);
+            }
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            String json = JsonConvert.DeserializeObject<String>(File.ReadAllText(pathToJSON));
+            News post = JsonConvert.DeserializeObject<News>(json);
+
+            textBox1.AppendText(post.Article);
+            textBox2.AppendText(post.Content);
+
+            post.Links.ForEach(x => textBox3.AppendText("\n Cсылка:" + x + "\n"));
+            post.Source.ForEach(x => textBox3.AppendText("\n Путь к картинке:" + x + "\n"));
+        }  
+
+        }
+}
---
+++ SumkinHomework/News.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+
+
+
+
+namespace SumkinHomework
+{
+
+    class News
+    {
+        [JsonProperty("Article")]
+        public String Article { get; set; }
+
+        [JsonProperty("Content")]
+        public String Content { get; set; }
+
+        [JsonProperty("Links")]
+        public List<String> Links { get; set; }
+
+        [JsonProperty("Source")]
+        public List<String> Source { get; set; }
+
+        public News(IWebDriver Browser)
+        {
+            IWebElement postArticleFind = Browser.FindElement(By.CssSelector(".post.post_full h1.post__title span"));
+            IWebElement postContentFind = Browser.FindElement(By.CssSelector(".post.post_full .post__body.post__body_full .post__text"));
+
+            List<IWebElement> postLinksInContent = Browser.FindElements(By.CssSelector(".post.post_full .post__body.post__body_full .post__text a")).ToList();
+            List<IWebElement> postLinksToImages = Browser.FindElements(By.CssSelector(".post.post_full .post__body.post__body_full .post__text img")).ToList();
+
+            List<IWebElement> postSumLinks = postLinksInContent.Union(postLinksToImages).ToList();
+
+            List<string> postLinks = new List<string>();
+            List<string> postSourceImg = new List<string>();
+
+            foreach (IWebElement links in postSumLinks)
+            {
+                if (links.GetAttribute("href") != null)
+                {
+                    postLinks.Add(links.GetAttribute("href"));
+                }
+                else if (
+                    links.GetAttribute("src") != null ||
+                    links.GetAttribute("src") != ""
+                )
+                {
+                    postSourceImg.Add(links.GetAttribute("src"));
+                }
+            }
+
+            Article = postArticleFind.Text;
+            Content = postContentFind.Text;
+            Source = postSourceImg;
+            Links = postLinks;
+
+        }
+    }
+}
